da638dbd8166dd72e07e8ff312283eaf9301bb8c
Shane <shanepopowski@yahoo.com>
2023-03-30 18:36:51 -0500

    added my portfolio for assets

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Welcome() {
+ 
+  return (
+    <div className="container">
+      <h1>Hello students!</h1>
+      <p>Today we will be learning about the following:</p>
+      <ul className="parent">
+        <li>React</li>
+        <li>Components</li>
+        <li>Props</li>
+        <li>Babel</li>
+      </ul>
+    </div>
+  );
+}
