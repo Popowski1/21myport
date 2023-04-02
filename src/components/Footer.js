43676ba2502e4790a22673347c1a4a64a64b8b2c
Shane <shanepopowski@yahoo.com>
2023-04-02 14:03:22 -0500

    update files struc

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Footer() { return (
+                    <footer className="footer"><p><b>Shane Popowski github</b>
+<a href="https://github.com/Popowski1?tab=repositories"> Github !</a></p></footer>);}
+export default Footer;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Navigation(props) {
-    const tabs = ["About","portfolio", "Contact", "Resume"];
+    const tabs = ["About","Portfolio", "Contact", "Resume"];
     return (
         <div className="tabs">
             <ul className="nav">
                 {tabs.map((tab) => ( <li className = { props.currentPage === tab ? "nav-item is-active" : "nav-item"}
                 key={tab}> <a href= {"#" + tab.toLowerCase()} onClick = {() => props.handlePageChange(tab)}
-                classname = { props.currentPage ===tab ? "nav-link active" : "nav-link"}></a>
+                classname = { props.currentPage === tab ? "nav-link active" : "nav-link"}></a>
                 </li>
                 ))}
             </ul>
